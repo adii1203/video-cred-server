21986683154544f93c29e96a92c331d73bbf5242
adii1203 <aditya32ft@gmail.com>
2025-09-20 23:12:49 +0530

    feat: add database connect - db.go

---
+++ cmd/db.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func InitDB() *pgx.Conn {
+	cnf, err := pgx.ParseConfig(os.Getenv("DATABASE_URI"))
+	if err != nil {
+		log.Fatal("Error: could not parse db uri")
+	}
+
+	db, err := pgx.ConnectConfig(context.Background(), cnf)
+	if err != nil {
+		log.Fatal("Error: database connection fail")
+	}
+
+	return db
+}
